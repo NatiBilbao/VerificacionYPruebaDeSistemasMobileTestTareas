75cef3db48bc28a77e9db30a9f35396f96ab864b
NatyBilbao <natabilbaocano18@gmail.com>
2024-10-31 23:05:33 -0400

    Tarea 17 - Crear actualizar y eliminar una tarea en WhenDo

--- src/test/java/tarea17/session/Session.java
+++ src/test/java/tarea17/session/Session.java
@@ -2,13 +2,12 @@ package tarea17.session;
 
 import io.appium.java_client.AppiumDriver;
 import tarea17.factoryDevices.FactoryDevices;
-import tarea17.utils.GetEnvironment;
 
 public class Session {
     private static Session session = null;
     private AppiumDriver device;
     private Session(){
-        // todo
+
         device = FactoryDevices.make("android").create();
     }
 
